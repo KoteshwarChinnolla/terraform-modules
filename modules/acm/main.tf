3cd1d43aaf1023fdab21b4a666d0805d8f3a2531
CHINNOLLA KOTESHWAR <98650844+KoteshwarChinnolla@users.noreply.github.com>
2025-09-11 06:50:39 +0000

    added

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -2,6 +2,7 @@ resource "aws_acm_certificate" "cert_default" {
   count             = var.cirtificate_exists || var.root_domine != null ? 0 : 1
   domain_name       = var.domine_name
   validation_method = var.validation_method
+  subject_alternative_names = var.alternative_domine_names
 
   tags = {
     Environment = var.Environment
@@ -31,19 +32,19 @@ locals {
 }
 
 data "aws_route53_zone" "selected" {
-  name         = var.root_domine != null ? var.root_domine : var.domine_name
-#   private_zone = true
+  name = var.root_domine != null ? var.root_domine : var.domine_name
 }
 
-resource "aws_route53_record" "example" {
-  for_each = {
+# Create validation records for the first certificate in locals
+resource "aws_route53_record" "validation" {
+  for_each = length(local.acm_certs) > 0 ? {
     for dvo in try(local.acm_certs[0].domain_validation_options, []) :
     dvo.domain_name => {
       name   = dvo.resource_record_name
       record = dvo.resource_record_value
       type   = dvo.resource_record_type
     }
-  }
+  } : {}
 
   allow_overwrite = true
   name            = each.value.name
@@ -53,8 +54,12 @@ resource "aws_route53_record" "example" {
   zone_id         = data.aws_route53_zone.selected.zone_id
 }
 
+resource "aws_acm_certificate_validation" "validation" {
+  count                   = length(local.acm_certs) > 0 ? 1 : 0
+  certificate_arn         = local.acm_certs[0].arn
+  validation_record_fqdns = [for record in aws_route53_record.validation : record.fqdn]
+}
 
-# Lookup existing cert if it already exists (ISSUED state)
 data "aws_acm_certificate" "issued" {
   domain      = var.domine_name
   statuses    = ["ISSUED"]
@@ -66,32 +71,36 @@ output "certificate_id" {
 }
 
 variable "cirtificate_exists" {
-  type = bool
+  type        = bool
   description = "true to use already existing cirtificate, false to creat a new cirtificate"
-  default = false
+  default     = false
 }
 
 variable "root_domine" {
-  type = string
-  description = "mection if any root domine exists so that CNAMES are not needed to be incerted again"
-  default = null
+  type        = string
+  description = "mention if any root domine exists so that CNAMES are not needed to be inserted again"
+  default     = null
 }
 
 variable "domine_name" {
-  type = string
-  description = "mection the name of the domine to wich the cirtificate is for"
-  default = null
+  type        = string
+  description = "mention the name of the domine to which the cirtificate is for"
+  default     = null
+}
+
+variable "alternative_domine_names" {
+  type    = list(string)
+  default = []
 }
 
 variable "validation_method" {
-  type = string
+  type        = string
   description = "It can be one of EMAIL or DNS"
-  default = "DNS"
+  default     = "DNS"
 }
 
-
 variable "Environment" {
-  type = string
+  type        = string
   description = "it can be anything dev, test, prod .."
-  default = "prod"
+  default     = "prod"
 }
