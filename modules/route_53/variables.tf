a7b9935b90527134b03a1c566c523d838e08a52d
CHINNOLLA KOTESHWAR <98650844+KoteshwarChinnolla@users.noreply.github.com>
2026-01-13 10:37:31 +0530

    sync with .dev mode

---
+++ modules/route_53/variables.tf
@@ -0,0 +1,16 @@
+variable "access_key" {
+  type = string
+}
+
+variable "secret_key" {
+  type = string
+}
+
+variable "load_balencer" {
+  type = string
+}
+
+variable "domine_name" {
+  type    = string
+  default = "anasolconsultancyservices.com" 
+}
